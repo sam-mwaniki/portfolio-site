ab38cbe4fb558e6c5d9406d6ad260d8ed22a5ceb
nisguy <magurumwaniki@gmail.com>
2019-02-09 23:18:55 +0300

    Updates

---
+++ precache-manifest.24cfd86afba82951e66e57ed9d2e5517.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "26e5d437f41e8da866a3",
+    "url": "/portfolio-site/static/js/runtime~main.26e5d437.js"
+  },
+  {
+    "revision": "eca5417a900cc1e5d8d5",
+    "url": "/portfolio-site/static/js/main.eca5417a.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/portfolio-site/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "eca5417a900cc1e5d8d5",
+    "url": "/portfolio-site/static/css/main.9045cfc8.chunk.css"
+  },
+  {
+    "revision": "87889bedcfdcdec316f9ba13bc7eae98",
+    "url": "/portfolio-site/index.html"
+  }
+];
